99ab20584c6652b62a8dea97e6b8384698c160e0
Pauloalex <pauloalexbarbosa22@gmail.com>
2023-08-02 17:35:49 -0300

    exercicios

--- exercicios/ex01.js
+++ exercicios/ex01.js
@@ -1,7 +1,16 @@
-//const nome = prompt('Qula é o seu nome');
+/*const nome = prompt('Qula é o seu nome');
 const n1 = Number.parseInt(prompt('Digite um numero:'));
 const n2 = Number.parseInt(prompt('Digite outro numero:'));
 const s = n1 + n2 ;
 alert(`O resultado é ${s} ` )
-//alert('É um prazer teconhecer '+ nome + ' !')
+//alert('É um prazer teconhecer '+ nome + ' !')*/
 
+console.log('Digite sua senha')
+var senha = '1234';
+
+
+if (senha == 1234) {
+    console.log(`Bem vindo  !!`)
+} else {
+    console.log(`Acesso negado`)
+}
