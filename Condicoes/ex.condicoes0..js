88d945c2c927600dc81dbb24cba4d8c4d657cc73
Pauloalex <pauloalexbarbosa22@gmail.com>
2023-08-08 19:57:06 -0300

    atualização

--- Condicoes/ex.condicoes0..js
+++ Condicoes/ex.condicoes0..js
@@ -1,17 +1,20 @@
 //           condições aninhadas
 
-var idade = 52
+var idade = 200
 
-if ( idade > 18 && idade <  50) {
+if ( idade >= 18 && idade <  50) {
     console.log('Voce é jovem')
 } else {
-    if (idade > 10 && idade < 18) {
+    if (idade >= 10 && idade < 18) {
         console.log('Voce é adolescente')
     }
-    if (idade > 50 ) {
+    if (idade >= 50 && idade <= 100) {
         console.log('Voce é velho')
     }
     if (idade < 10) {
         console.log('Voce é uma criança')
+    }
+    if (idade > 100) {
+        console.log('Voce ja morreu a muito tempo kkkk')
     }
 }
