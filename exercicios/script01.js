54380121b3fcd5dbe41ac3a6d4e2be3ed86746a6
Pauloalex <pauloalexbarbosa22@gmail.com>
2023-08-11 20:32:30 -0300

    atualizar

--- exercicios/script01.js
+++ exercicios/script01.js
@@ -1,3 +1,25 @@
-function carregar() {
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
     
-}
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] verifique os dados e tente novamente!')
+
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            res.innerHTML = `Você é um ${genero}, e tem ${idade} anos`
+            
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            res.innerHTML = `Você é uma ${genero}, e tem ${idade} anos`
+        }
+       
+    }
+
+} 
